457a168c2017387311d4b91ba110b655bec14954
Vignesh Krishnamoorthy <vignesh@hybridgreentech.com>
2021-04-29 15:02:06 +0200

    Add a shipit file for shipping dagit to ecr

---
+++ shipit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+REGION=eu-north-1
+VERSION=$1
+AWS_PATH=$2
+
+docker build . -f Dockerfile_dagit -t dagster/dagit_service:$VERSION
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_PATH
+
+docker tag dagster/dagit_service:$VERSION $AWS_PATH/dagster/dagit_service:$VERSION
+
+docker push $AWS_PATH/dagster/dagit_service:$VERSION
